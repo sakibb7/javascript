66b703992997fcb6df40e44dabfb0c0427be5b19
Mohammad Sakib <sakibb076@gmail.com>
2023-09-10 14:53:15 +0600

    add string methods

---
+++ fundamentals/stringMethods.js
@@ -0,0 +1,18 @@
+"Hello".charAt(4); //output o
+"Hello".concat(" ", "world"); //output "Hello world"
+"Hello".startsWith("H"); //output true
+"Hello".endsWith("o"); //output true
+"Hello".includes("x"); //output false
+"Hello".indexOf("o"); //output 4
+"Hello".lastIndexOf("l"); //output 3
+"Hello".match(/[A-Z]/g); //output H
+"Hello".repeat(3); //output HelloHelloHello
+"Hello".replace("llo", "sakib"); //output Hesakib
+"Hello".search("e"); //output 1
+"Hello".slice(2, 4); //output ll
+"Hello".split(""); //output ['H','e','l','l','o']
+"Hello".toLowerCase(); //output hello
+"Hello".toUpperCase(); //output HELLO
+" Hello ".trim(); //output Hello
+" Hello ".trimStart(); //output "Hello "
+" Hello ".trimEnd(); //output " Hello"
